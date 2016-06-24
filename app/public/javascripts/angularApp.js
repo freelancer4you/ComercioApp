576929c23c9ff1dc1d142e26222cb04cf6d28d98
goldi <goldi23@freenet.de>
2016-06-24 12:43:28 +0200

    Using angularjs for navigation

--- app/public/javascripts/angularApp.js
+++ app/public/javascripts/angularApp.js
@@ -1,7 +1,40 @@
-var app = angular.module('comercioApp', []);
+var app = angular.module('comercioApp',  ['ngRoute']);
 
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
   $scope.test = 'Hello worc!';
 }]);
+
+app.controller('TodoController', ['$scope',function ($scope) {
+    
+  }]);
+app.controller('AboutCtrl', ['$scope', function ($scope) {
+    
+  }]);
+app.controller('ContactCtrl', ['$scope', function ($scope) {
+    
+}]);
+
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '/todos.html',
+        controller: 'TodoController'
+      })
+      /*.when('/:id', {
+        templateUrl: '/todoDetails.html',
+        controller: 'TodoDetailCtrl'
+     })*/
+     .when('/about', {
+        templateUrl: '/about.html',
+        controller: 'AboutCtrl'
+     })
+     .when('/contact', {
+        templateUrl: '/contact.html',
+        controller: 'ContactCtrl'
+     })
+     ;
+     
+  }]);
